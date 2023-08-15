a3647b8297217e3bd497b8ae73e5e9d8d0e8a0bc
Margarita <te120883ama@gmail.com>
2023-08-15 19:43:12 +0200

    Add submit event to form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
+'use strict'
+const messageArray = [];
+
+const textForm = document.getElementById('textForm');
+// const textForm = document.forms.textForm;
+const textInput = textForm.textInput;
+
+textForm.addEventListener('submit', (event)=>{
+  event.preventDefault();
+  // console.log(event.target.elements[1]);
+  // console.log(event.target.elements.textInput);
+  const {target: {textInput:{value: valuetextInput} }} = event;
+  console.log(valuetextInput);
+});
+
+
 
