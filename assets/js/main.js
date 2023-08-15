93533afcb7216997b0244d83db28a1212cad94f6
Margarita <te120883ama@gmail.com>
2023-08-15 21:28:00 +0200

    Add massageArray

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,36 @@
-'use strict'
+"use strict";
 const messageArray = [];
 
-const textForm = document.getElementById('textForm');
-// const textForm = document.forms.textForm;
-const textInput = textForm.textInput;
+const textForm = document.getElementById("textForm");
+const messages = document.getElementById("messages");
 
-textForm.addEventListener('submit', (event)=>{
+textForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  // console.log(event.target.elements[1]);
-  // console.log(event.target.elements.textInput);
-  const {target: {textInput:{value: valuetextInput} }} = event;
-  console.log(valuetextInput);
+
+  const {
+    target: {
+      textInput: { value: valuetextInput },
+    },
+  } = event;
+
+  const prepareValue = valuetextInput.trim();
+  if (prepareValue && !messageArray.includes(prepareValue)) {
+    event.target.textInput.style.backgroundColor = "";
+    messageArray.push(prepareValue);
+    const pMessage = createElement(
+      "p",
+      {},
+      document.createTextNode(prepareValue)
+    );
+    messages.append(pMessage);
+    console.log(messageArray);
+    textForm.reset();
+  } else {
+    event.target.textInput.style.backgroundColor = "pink";
+  }
 });
 
+// const user = "Петренко А. О";
 
-
+// const checkUserPattern = /[А-Я][а-я]*/;
+// console.log(checkUserPattern);
