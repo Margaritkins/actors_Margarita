53e4f6390b8e47cda29e913a4429214294776e8c
Margarita <te120883ama@gmail.com>
2023-08-16 14:08:04 +0200

    Add setInterval task resolve

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,17 @@
 "use strict";
-const usersArray = [];
 
-const textForm = document.getElementById("textForm");
-const messages = document.getElementById("messages");
-
-textForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const {
-    target: {
-      textInput: { value: valuetextInput },
-    },
-  } = event;
-
-  const prepareValue = valuetextInput.trim();
-  if (checkUserPattern.test(prepareValue) && !usersArray.includes(prepareValue)) {
-    event.target.textInput.style.backgroundColor = "";
-    usersArray.push(prepareValue);
-    const pMessage = createElement(
-      "p",
-      {},
-      document.createTextNode(prepareValue)
-    );
-    messages.append(pMessage);
-    console.log(usersArray);
-    textForm.reset();
-  } else {
-    event.target.textInput.style.backgroundColor = "pink";
+function countdown (){
+  const number = document.getElementById("text");
+// console.dir(number.innerText);
+let count = 10;
+const idInterval = setInterval(() => {
+  count--
+  number.innerText = count
+  if (count <= 0) {
+    number.innerText = 'counter stopped'
+    clearInterval(idInterval);
   }
-});
+}, 1000);
+}
 
-const user = "Петренко А.О";
-
-// const checkUserPattern = /^[А-Я][а-я]* [А-Я]\. [А-Я]\.$/;
-const checkUserPattern = /^[А-Я][а-я]* ([А-Я]\.){2}$/g;
-console.log(usersArray);
+countdown()
